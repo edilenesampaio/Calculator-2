1715c89037bdac0571fb7920ff90c099b7772caa
Nicole Yeager <nyeager95@gmail.com>
2023-11-29 17:43:38 -0700

    Subtraction Repl

--- calculator.py
+++ calculator.py
@@ -22,6 +22,11 @@ while True:
     elif "+" in tokens:
         print(add(float(num1),float(num2)))
 
+    elif "-" in tokens:
+        print(subtract(float(num1),float(num2)))
+        
+
+
         
 #             (decide which math function to call based on first token)
 #             if the first token is 'pow':

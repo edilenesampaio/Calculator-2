7d700d23acaa97711ad5318c8292e74251f8df17
Nicole Yeager <nyeager95@gmail.com>
2023-11-29 17:36:55 -0700

    Add repl

--- calculator.py
+++ calculator.py
@@ -5,3 +5,26 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
+# repeat forever:
+while True:
+    #     read input
+    user_input = input("> ")
+#     tokenize input
+    tokens = user_input.split(" ")
+
+    num1 = tokens[1]
+    num2 = tokens[2]
+#         if the first token is "q":
+    if "q" in tokens:
+        print("Quitting game")
+        break
+
+    elif "+" in tokens:
+        print(add(float(num1),float(num2)))
+
+        
+#             (decide which math function to call based on first token)
+#             if the first token is 'pow':
+#                   call the power function with the other two tokens
+
+#             (...etc.)
